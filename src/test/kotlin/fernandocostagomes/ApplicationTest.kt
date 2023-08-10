3c6f25b50e9a461b6155b7c2b0e96d2734c94247
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-08-10 13:46:22 -0300

    Alterado a string na applicationTest novamente.

--- src/test/kotlin/fernandocostagomes/ApplicationTest.kt
+++ src/test/kotlin/fernandocostagomes/ApplicationTest.kt
@@ -15,7 +15,7 @@ class ApplicationTest {
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("ApiServer V+!!", bodyAsText())
+            assertEquals("ApiServer V+!", bodyAsText())
         }
     }
 }
