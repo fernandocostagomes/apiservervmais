c5292cbce971afb7e2f91fcf5da2b1a88f6ea9c4
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-09-14 14:51:48 -0300

    Alterado o ip do banco de dados para dominio

--- src/main/kotlin/fernandocostagomes/plugins/PluginDatabases.kt
+++ src/main/kotlin/fernandocostagomes/plugins/PluginDatabases.kt
@@ -50,7 +50,7 @@ fun Application.configureDatabases() {
  * */
 fun connectToPostgres(embedded: Boolean): Connection {
 
-    val url = "172.17.0.3"
+    val url = "fcg_postgres"
     val db = "dbfcgvmais"
     val port = "5432"
     val user = "userfcgpostgres"
