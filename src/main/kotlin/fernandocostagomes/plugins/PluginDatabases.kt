9fdc292486f1fe64d04c20d452f95822c34c46c3
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-09-14 15:14:05 -0300

    Alterado corretamente o nome do banco de dados

--- src/main/kotlin/fernandocostagomes/plugins/PluginDatabases.kt
+++ src/main/kotlin/fernandocostagomes/plugins/PluginDatabases.kt
@@ -50,7 +50,7 @@ fun Application.configureDatabases() {
  * */
 fun connectToPostgres(embedded: Boolean): Connection {
 
-    val url = "3c6eb778b8c9"
+    val url = "fcg_postgres"
     val db = "dbfcgvmais"
     val port = "5432"
     val user = "userfcgpostgres"
