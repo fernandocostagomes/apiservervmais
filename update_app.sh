d6a81c693a53e46abdf9ec6f7e3f299abba4a7c0
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-09-18 08:51:33 -0300

    Corrigido o script compose do app.

--- update_app.sh
+++ update_app.sh
@@ -12,11 +12,14 @@ gradle build
 # Passo 4: Construir a imagem do Docker
 docker buildx build . -t fcg_webapp
 
-# Passo 5: Parar o container fcg_webapp
-docker stop fcg_webapp
+# Verifica se o container existe
+docker ps -a | grep fcg_webapp
 
 # Passo 6: Remover o container fcg_webapp
-docker rm fcg_webapp
+docker rm --force fcg_webapp
+
+# Simula a remocao do container
+docker rm --force dry-run fcg_webapp
 
 # Passo 7: Rodar o compose que para executar o container fcg_webapp
 docker-compose up -d --services fcg_webapp
