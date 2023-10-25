a22c88271fb9dab6a2437dae6aa9a20f8450acf7
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-10-25 10:15:41 -0300

    Alteardo comandos de escrita no script build all.

--- server_app_update.sh
+++ server_app_update.sh
@@ -1,88 +1,88 @@
 #!/bin/bash
 
 # Passo 1: Atualizar o repositório local
-Write-Output "###############"
-Write-Output "### Atualizando o repositório local com 'git pull'. ###"
+echo "###############"
+echo "### Atualizando o repositório local com 'git pull'. ###"
 git pull
-Write-Output "### Finalizado. ###"
+echo "### Finalizado. ###"
 
 # Passo 2: Limpar o cache do Gradle
-Write-Output "###############"
-Write-Output "### Limpando o projeto com 'gradle clean'. ###"
+echo "###############"
+echo "### Limpando o projeto com 'gradle clean'. ###"
 gradle clean
-Write-Output "### Finalizado. ###"
+echo "### Finalizado. ###"
 
 # Passo 3: Compilar o código
-Write-Output "###############"
-Write-Output "### Construindo o projeto com 'gradle build'. ###"
+echo "###############"
+echo "### Construindo o projeto com 'gradle build'. ###"
 gradle build
 
 # Passo 4: Construir a imagem do Docker
-Write-Output "###############"
-Write-Output "### Construindo a imagem do Docker com 'docker buildx build . -t fcg_webapp'. ###"
+echo "###############"
+echo "### Construindo a imagem do Docker com 'docker buildx build . -t fcg_webapp'. ###"
 docker buildx build . -t fcg_webapp
-Write-Output "### Finalizado. ###"
+echo "### Finalizado. ###"
 
 # Passo 5: Construir o container de acordo com o arquivo compose, antes excluindo as redes e parando os
 # containers se estiverem iniciados ou criados.
-Write-Output "###############"
-Write-Output "### Construindo o container com 'docker-compose up -d --remove-orphans'. ###"
+echo "###############"
+echo "### Construindo o container com 'docker-compose up -d --remove-orphans'. ###"
 docker-compose down
 docker-compose up -d --remove-orphans
-Write-Output "### Finalizado. ###"
+echo "### Finalizado. ###"
 
 # Passo 6: Removendo os containers que não estão em uso da aplicacao web.
-Write-Output "###############"
-Write-Output "### Removendo os containers que não estão em uso da aplicacao web. ###"
+echo "###############"
+echo "### Removendo os containers que não estão em uso da aplicacao web. ###"
 docker container prune -f
-Write-Output "### Finalizado. ###"
+echo "### Finalizado. ###"
 
 # Passo 7: Aguardar 10 segundos para que o container seja iniciado.
-Write-Output "### Aguardando inicialização dos servidores ###"
-Write-Output "##########"
+echo "### Aguardando inicialização dos servidores ###"
+echo "##########"
 Start-Sleep -s 1
-Write-Output "#########"
+echo "#########"
 Start-Sleep -s 1
-Write-Output "########"
+echo "########"
 Start-Sleep -s 1
-Write-Output "#######"
+echo "#######"
 Start-Sleep -s 1
-Write-Output "######"
+echo "######"
 Start-Sleep -s 1
-Write-Output "#####"
+echo "#####"
 Start-Sleep -s 1
-Write-Output "####"
+echo "####"
 Start-Sleep -s 1
-Write-Output "###"
+echo "###"
 Start-Sleep -s 1
-Write-Output "##"
+echo "##"
 Start-Sleep -s 1
-Write-Output "#"
+echo "#"
 Start-Sleep -s 1
-Write-Output "##"
+echo "##"
 Start-Sleep -s 1
-Write-Output "###"
+echo "###"
 Start-Sleep -s 1
-Write-Output "####"
+echo "####"
 Start-Sleep -s 1
-Write-Output "#####"
+echo "#####"
 Start-Sleep -s 1
-Write-Output "######"
+echo "######"
 Start-Sleep -s 1
-Write-Output "#######"
+echo "#######"
 Start-Sleep -s 1
-Write-Output "########"
+echo "########"
 Start-Sleep -s 1
-Write-Output "#########"
+echo "#########"
 Start-Sleep -s 1
-Write-Output "##########"
+echo "##########"
 Start-Sleep -s 1
-Write-Output "###########"
+echo "###########"
 Start-Sleep -s 1
 
 # Passo 8: Popular o banco de dados com o script em python que esta no diretorio /config/init_db.py se os passos
 # anteriores estiverem sido executados com sucesso.
-Write-Output "###############"
-Write-Output "### Populando o banco de dados com 'Python init_db.py'. ###"
+echo "###############"
+echo "### Populando o banco de dados com 'Python init_db.py'. ###"
 Invoke-Expression "py init_db.py localhost"
-Write-Output "### Finalizado. ###"
+echo "### Finalizado. ###"
