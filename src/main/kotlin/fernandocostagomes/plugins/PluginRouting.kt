06d4d9dd64e79435f9f1b181d7e1b622b520f0ca
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-10-26 10:42:15 -0300

    Config url update.

--- src/main/kotlin/fernandocostagomes/plugins/PluginRouting.kt
+++ src/main/kotlin/fernandocostagomes/plugins/PluginRouting.kt
@@ -5,13 +5,16 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.joda.time.LocalDateTime
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("ApiServer V+ atualizada!")
+            call.respondText("ApiServer V+ atualizada!\n" +
+                    //pega a data e hora.
+                   "Data e hora: ${LocalDateTime.now()}\n")
         }
         // Rota que aciona o script server_app_update.sh que esta na raiz do projeto para atualizar o servidor,
         // solicita uma senha para confirmar.
