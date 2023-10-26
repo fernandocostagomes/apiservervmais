1cf63267e04238965980530bd1ca8faaae6a4a50
Fernando Gomes <fernando.gomes@autotrac.com.br>
2023-10-26 09:27:09 -0300

    Config url shutdown.

--- src/main/kotlin/fernandocostagomes/plugins/PluginAdministration.kt
+++ src/main/kotlin/fernandocostagomes/plugins/PluginAdministration.kt
@@ -7,6 +7,8 @@ fun Application.configureAdministration() {
     install(ShutDownUrl.ApplicationCallPlugin) {
         // The URL that will be intercepted (you can also use the application.conf's ktor.deployment.shutdown.url key)
         shutDownUrl = "/ktor/application/shutdown"
+        //imprimindo no console
+        println("ShutDownUrl.ApplicationCallPlugin")
         // A function that will be executed to get the exit code of the process
         exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
     }
