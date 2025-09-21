ce6c262fcc86a22d8c58476b9cd1a17077c2cd7d
Fernando <fernandocostagomes@gmail.com>
2025-09-20 23:23:37 -0300

    Feature sistematizacao.

--- src/main/kotlin/fernandocostagomes/routes/Teste2.kt
+++ src/main/kotlin/fernandocostagomes/routes/Teste2.kt
@@ -1,8 +1,10 @@
 package fernandocostagomes.routes
 
-class Teste {
+class Teste2 {
     val id: Int = 0
     val teste: String = "Teste"
     val teste1: String = "Teste1"
     val teste2: String = "Teste2"
+    val teste3: String = "Teste3"
+    val teste4: String = "Teste4"
 }
